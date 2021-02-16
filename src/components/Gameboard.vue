3e9a9ac4c1e17bb53f92083b263d98c86f48f290
Piotr Wrobel <piotr.wrobel@hotmail.com>
2021-02-16 23:33:18 +0100

    build gameboard component

--- src/components/Gameboard.vue
+++ src/components/Gameboard.vue
@@ -1,7 +1,8 @@
 <template>
-  <span>
+  <div class="gameboard">
     <slot/>
-  </span>
+    <div class="tile-slot" v-for="(item,index) in 16" :key="index"></div>
+  </div>
 </template>
 
 <script>
@@ -10,5 +11,13 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+  .gameboard {
+    @apply relative bg-brown-400 rounded-lg grid grid-rows-4 grid-flow-col gap-4 p-4;
+    width: 500px;
+    height: 500px;
+  }
 
+  .tile-slot {
+    @apply rounded bg-brown-300;
+  }
 </style>
